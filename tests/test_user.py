f3fdfad8fd6e5af5e91a1e6ba37c4e544f85c026
PedroRibeiro <pedroribeiro89@gmail.com>
2023-04-10 21:15:15 -0300

    test post

--- tests/test_user.py
+++ tests/test_user.py
@@ -18,3 +18,27 @@ def test_list_users():
     assert response.status_code == 200
     assert result == expect
 
+
+def test_add_user():
+    expected_id = -1
+    for db_user in user_data:
+        if db_user['id'] > expected_id:
+            expected_id = db_user['id']
+
+    data = {
+        "name": "Pedro Soares",
+        "phone": "(31) 99999-9999",
+        "email": "psoares@teste.com",
+        "country": "Brazil"
+    }
+    response = api.test_client().post('/users', json=data)
+    print(response)
+    user = json.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 200
+    assert user['name'] == 'Pedro Soares'
+    assert user['phone'] == '(31) 99999-9999'
+    assert user['email'] == 'psoares@teste.com'
+    assert user['country'] == 'Brazil'
+    assert user['id'] == expected_id + 1
+
